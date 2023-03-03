71bfd87dd664361733dea5e37d4fa5f7f97c37a1
Steve Dytewski <dy2ski@gmail.com>
2023-03-02 18:14:30 -0800

    added code for map and returning list, have error about key

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,14 +19,54 @@ const Item = styled(Paper)(({ theme }) => ({
 function Footer() {
     // Declare a new state variable, which we'll call "count"
     const [task, setTask] = useState("");
+    const [taskList, setTaskList] = useState([]);
+    const [completedTaskCount, setCompletedTaskCount] = useState(0);
+
+
   
+    
       // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
-    document.title = `You clicked ${task} times`;
-    
-  });
+    document.title = `You clicked ${completedTaskCount} times`;
+
+
   
+  })
+  
+  
+  const handleClick = () => {
+    const id = taskList.length + 1;
+    setTaskList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: task,
+        complete: false,
+      },
+    ]);
+    setTask("");
+  };         
+  
+  const handleComplete = (id) => {
+    let list = setTaskList.map((task) => {
+      let item = {};
+      if (task.id == id) {
+        if (!task.complete){
+            //Task is pending, modifying it to complete and increment the count
+            setCompletedTaskCount(completedTaskCount + 1);
+        } 
+        else {
+            //Task is complete, modifying it back to pending, decrement Complete count
+            setCompletedTaskCount(completedTaskCount - 1);
+        }
+    item = { ...task, complete: !task.complete };
+      } else item = { ...task };
+  
+      return item;
+    });
+    setTaskList(list);
+  };
   
     return (
       
@@ -35,25 +75,53 @@ function Footer() {
        <Grid container spacing={2}>
      
          <Grid item xs={8}>
-           <Item> <Button  variant="contained" onClick={() => setTask(task + 1)}>Click me</Button></Item>
+           <Item> <Button  variant="contained" onClick={() => setCompletedTaskCount(completedTaskCount + 1)}>Click me</Button></Item>
          </Grid>
          <Grid item xs={4}>
            <Item>
             
             <div>
-        <p>Add a new {task} times</p>
+        <p>Add a new {completedTaskCount} times</p>
        
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" type="text" name="task" value={task} />
+        <TextField  onInput={(e) =>setTask(e.target.value)} id="outlined-basic" label="Outlined" variant="outlined" type="text" value="task" />
         <Button variant="outlined" startIcon={<DeleteIcon />}>
          Delete
             </Button>   
-        <Button variant="contained" endIcon={<SendIcon />}>
+        <Button onClick={() => handleClick()} variant="contained" endIcon={<SendIcon />}>
              Send
             </Button>
       </div>
-      
-      </Item>
+      <Grid item xs={6}>
+          <h2>Completed tasks</h2>
          </Grid>
+         <Grid item xs={6}>
+         <h2>Pending tasks</h2>
+            </Grid>
+      </Item>
+      <Grid item xs={12}>
+
+        <ul>
+        {taskList.map((todo) => {
+    return (
+      <p
+        complete={task.complete}
+        id={task.id}
+        onClick={() => handleComplete(task.id)}
+        style={{
+          listStyle: "none",
+          textDecoration: todo.complete && "line-through",
+        }}
+      >
+        {task.task}
+      </p>
+    );
+  })}
+
+        </ul>
+      </Grid>
+      
+         </Grid>
+
          
        </Grid>
      </Box>
